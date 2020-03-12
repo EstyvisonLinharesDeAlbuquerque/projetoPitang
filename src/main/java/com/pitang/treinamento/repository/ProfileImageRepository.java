c21415cab3e5a9b7e64bb4d5b92264f61c39174e
Estyvison Linhares <ela@ecomp.poli.br>
2020-03-12 10:42:23 -0300

    ImageProfile faltando alguns ajustes

--- src/main/java/com/pitang/treinamento/repository/ProfileImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pitang.treinamento.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pitang.treinamento.model.ProfileImageModel;
-
-public interface ProfileImageRepository extends JpaRepository<ProfileImageModel, Long>{
-
-}
--- src/main/java/com/pitang/treinamento/service/ProfileImageService.java
+++
@@ -1,6 +0,0 @@
-package com.pitang.treinamento.service;
-import com.pitang.treinamento.model.ProfileImageModel;
-
-public interface ProfileImageService {
-	public ProfileImageModel addProfileImage(Long id);
-}
